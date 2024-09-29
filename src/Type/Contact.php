cf31c3e5aa8d2a3ca1e82a02add62b3740ee02b3
Anton <anton.z@live.com>
2024-09-29 20:11:56 +0800

    Bugfix: Contact lastName must be optional

--- src/Type/Contact.php
+++ src/Type/Contact.php
@@ -25,10 +25,12 @@ final readonly class Contact extends Type
         /**
          * Optional. Contact's last name
          */
-        public string $lastName,
+        public string|null $lastName = null,
 
         /**
-         * Optional. Contact's user identifier in Telegram
+         * Optional. Contact's user identifier in Telegram.
+         * This number may have more than 32 significant bits and some programming languages may have difficulty/silent defects in interpreting it.
+         * But it has at most 52 significant bits, so a 64-bit integer or double-precision float type are safe for storing this identifier.
          */
         public int|null $userId = null,
 
